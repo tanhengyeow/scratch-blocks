f49dadfa1efd92b219e2e8993dd0fce6ec034df8
iNekox3 <inekox3@hotmail.com>
2019-11-04 23:32:08 +0800

    Implement finish event

--- experiment/logEvents.js
+++ experiment/logEvents.js
@@ -38,7 +38,6 @@ function logEvents(workspace, VERSION, ANSWER) {
     var date = new Date();
     var data = [step++, element, x, y, date, event.target];
     actions.push(data);
-    //console.log(actions);
 
     totalClicks++;
   }, true);
@@ -61,86 +60,36 @@ function logEvents(workspace, VERSION, ANSWER) {
     if (!(uniqueIdentifier in blocksID) && uniqueIdentifier != lastClickedBlock) {
       blocksID[uniqueIdentifier] = lastClickedBlock;
     }
-    //console.log(blocksID);
-
-    /*if (isTaskComplete()) {
-      // Send logging info
-    }*/
-    console.log(isTaskComplete());
   }, true);
 
   // Check if task is completed, i.e. code block has been replicated
+  workspace.addChangeListener(isTaskComplete);
   function isTaskComplete() {
-    function isCode(obj) {
-      console.log(obj ? obj.type.split('_')[0] : "");
-      return obj && obj.type.split('_')[0] in categories;
-    }
-
     function searchAnswer(idx, block) {
       if (block == null) { return false; }
-      if (!isCode(block)) { return false; }
-      console.log(block, "is code block!");
     
       if (block.type === ANSWER[idx]) {
-        console.log('matches', ANSWER[idx]);
         // Is end of answer
+        var nextBlock = block.getNextBlock();
         if (idx+1 === ANSWER.length) {
-          console.log("end of comparison");
-          console.log(block.childBlocks_);
-          if (block.childBlocks_ === undefined) {
-            console.log("it has no children");
-            return true; 
+          if (!nextBlock) {
+            //sendLogData();
+            return true;
           }
-
-          for (var i = 0; i < block.childBlocks_.length; i++) {
-            console.log("ISCODEE", block.childBlocks_[i], isCode(block.childBlocks_[i]));
-            if (isCode(block.childBlocks_[i])) {
-              console.log("it is not the last block");
-              return false;
-            }
-          }
-          return true;
+          else { return false; }
         }
   
         // Continue comparison
-        if (!block.childBlocks_) {
-          console.log("it has no children");
-          return false;
-        }
-        for (let j = 0; j < block.childBlocks_.length; j++) {
-          var result = searchAnswer(idx+1, block.childBlocks_[j]);
-          if (result) {
-            console.log("complete!");
-            return true;
-          }
-        }
-
-        return false;
+        if (!nextBlock) { return false; }
+        return searchAnswer(idx+1, nextBlock);
       }
-      console.log("does not match", ANSWER[idx])
       return false;
     }
 
-    var blocks = workspace.getAllBlocks();
-    var categories = {
-      'motion': 'motion',
-      'looks': 'looks',
-      'sound': 'sound',
-      'events': 'events',
-      'control': 'control',
-      'sensing': 'sensing',
-      'operators': 'operators',
-    };
-
-    console.log("ALL BLOCKS", blocks);
+    var blocks = workspace.getTopBlocks();
     for (var i = 0; i < blocks.length; i++) {
-      console.log("checking block", blocks[i], blocks[i].parentBlock_);
-      if (isCode(blocks[i]) && !blocks[i].parentBlock_) {
-        console.log('/***************************/')
-        var isDone = searchAnswer(0, blocks[i]);
-        console.log("IS DONEEEEE!", isDone);
-        if (isDone) { return true; }
-      }
+      var isDone = searchAnswer(0, blocks[i]);
+      if (isDone) { return true; }
     }
 
     return false;
